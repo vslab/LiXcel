ce16024873c60c26f90d5919127f4aabfcb2f56b
Leonardo Bartoloni <bartolon@di.unipi.it>
2014-02-20 17:38:37 +0100

    Added test random functions

--- LiXcel/LiXcelCore/FunctionLibrary.fs
+++ LiXcel/LiXcelCore/FunctionLibrary.fs
@@ -2,5 +2,12 @@
 open Microsoft.Office.Interop
 
 type FunctionLibrary =
+    static member rng = new System.Random()
     static member SUM values =
         List.sum<float> values
+    static member LXLUNIFORM left right =
+        (FunctionLibrary.rng.NextDouble()) * (right - left) + left
+    static member LXLGAUSS m sigma = 
+        let u = FunctionLibrary.LXLUNIFORM 1.0 0.0
+        let v = FunctionLibrary.LXLUNIFORM 0.0 1.0
+        m + sigma * sqrt(-2. * log(u)) * cos(2. * System.Math.PI * v)
