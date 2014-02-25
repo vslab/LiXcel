a0c9f1fbd86d76a8b5af40e655e5e5f58837cb6f
Leonardo Bartoloni <bartolon@di.unipi.it>
2014-02-25 18:58:45 +0100

    script for testing compiler/parser

--- LiXcel/LiXcelCore/Script.fsx
+++ LiXcel/LiXcelCore/Script.fsx
@@ -1,8 +1,12 @@
 ﻿// Per ulteriori informazioni su F#, visitare http://fsharp.net. Vedere il progetto 'Esercitazione su F#'
 // per ulteriori linee guida sulla programmazione F#.
 
-#load "Library1.fs"
+#r @"C:\Users\pq\Documents\GitHub\LiXcel\LiXcel\LiXcelCore\bin\Debug\LiXcelCore.dll"
 open LiXcelCore
 
+let formula = "=SUM!A3"
+
+let q = Parser.tokenize formula
+let q = Parser.parseExpr formula
 // Viene definito qui il codice di script della libreria
 
