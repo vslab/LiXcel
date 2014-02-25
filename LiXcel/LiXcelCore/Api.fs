8cf09ff32fda09339aa06d3a606e9be0bf551e86
Leonardo Bartoloni <bartolon@di.unipi.it>
2014-02-25 18:37:12 +0100

    Added "simulate" function to core api
    montecarlo simulation api (to be used from ribbon)

--- LiXcel/LiXcelCore/Api.fs
+++ LiXcel/LiXcelCore/Api.fs
@@ -22,3 +22,26 @@ type Api ()=
         let expr = Parser.parseExpr (cell.Formula.ToString())
         let evaluation = Compiler.Compile cell expr
         evaluation().ToString() |>box
+
+    member this.Simulate (origin:Excel.Range) (destination:Excel.Range) (iterationCount:int) =
+        let bucketCount = destination.Rows.Count
+        let simulator =  Compiler.CompileCell origin 
+        let simulationResult = Array.init iterationCount (simulator<<ignore)
+        let rangeMin = Array.min simulationResult
+        let rangeMax = Array.max simulationResult
+        let range = rangeMax-rangeMin
+        let bucketWidth = if range = 0.0 then 1.0 else range/(float bucketCount)
+        let buckets =
+            simulationResult
+            |> Seq.countBy (fun x ->
+                min ((x-rangeMin) / bucketWidth|>truncate |> int ) (bucketCount - 1) )|> Seq.sort |> Seq.toList
+        let printer (bucketNumber,count) =
+                let imin = (float bucketNumber )*bucketWidth + rangeMin
+                let imax = imin + bucketWidth
+                let label = sprintf "%4f - %4f"  imin  imax
+                let labelCell :Excel.Range = destination.Item(1 + bucketNumber|>box,1|>box)|>unbox
+                let countCell :Excel.Range = destination.Item(1 + bucketNumber|>box,2|>box)|>unbox
+                labelCell.Formula <- label |> box
+                countCell.Formula <- sprintf "%d" count |> box
+        for  x in 0..(bucketCount-1) do printer (x, 0)
+        buckets |> Seq.iter printer
